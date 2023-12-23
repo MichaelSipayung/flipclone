91abe57a637472a733f9647b6c40f1e82600bb89
Michael Sipayung <Michaelsipayung123@gmail.com>
2023-12-23 22:34:51 +0700

    add improvement

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class MainController < ApplicationController
+  def index
+    #flash[:notice] = "Welcome to the about page!"
+    #flash[:alert] = "You are not allowed to access this page."
+  end
+end
